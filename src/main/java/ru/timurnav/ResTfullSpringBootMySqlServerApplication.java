583a0d5f88510ce61dd70c5d16f276afa14647a4
timurnav <timurnav@gmail.com>
2015-08-20 06:49:56 +0300

    empty project

---
+++ src/main/java/ru/timurnav/ResTfullSpringBootMySqlServerApplication.java
@@ -0,0 +1,12 @@
+package ru.timurnav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfullSpringBootMySqlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfullSpringBootMySqlServerApplication.class, args);
+    }
+}
