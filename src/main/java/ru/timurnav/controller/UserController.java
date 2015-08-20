1ab556a5eb4e7da92afe1721a0964b905419055d
timurnav <timurnav@gmail.com>
2015-08-20 09:53:34 +0300

    now we can get the entities from database via REST interface!

--- src/main/java/ru/timurnav/controller/UserController.java
+++ src/main/java/ru/timurnav/controller/UserController.java
@@ -1,19 +1,32 @@
 package ru.timurnav.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.timurnav.domain.User;
+import ru.timurnav.domain.UserRepository;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/users")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public User get(@PathVariable("id") Long id) {
 
-        return new User(id, "/pic/user"+id, "user", "user@yandex.ru");
+        return userRepository.findOne(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<User> getAll (){
+        return userRepository.findAll();
     }
 
 
